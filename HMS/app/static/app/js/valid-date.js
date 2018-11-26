6382145e4eb21d5301304efbd3053d589742f8a3
KameshwaraRao Mahendrawada <kameshsaim@outlook.com>
2018-11-26 18:44:11 +0530

    Javascript changes

--- HMS/app/static/app/js/valid-date.js
+++ HMS/app/static/app/js/valid-date.js
@@ -1,27 +1,31 @@
 // DatePicker
-$(function ()
-{
+$(function () {
     $("#datepicker_CIn").datepicker();
     $("#datepicker_COut").datepicker();
     //Pass the user selected date format
-    $("#format").change(function ()
-    {
+    $("#format").change(function () {
         $("#datepicker_CIn").datepicker("option", "dateFormat", $(this).val());
         $("#datepicker_COut").datepicker("option", "dateFormat", $(this).val());
     });
 });
-
-function handler(e)
-{
+function handler(e) {
     var today = new Date();
     var dd = today.getDate();
     var mm = today.getMonth() + 1; //January is 0!
     var yyyy = today.getFullYear();
     today = mm + '/' + dd + '/' + yyyy;
-    if (selected < today)
-    {
-        alert("Please check the dates you mentioned");
+    selected = e.target.value;
+    if (selected < today) {
+        alert("please selected above today date");
         $('#datepicker_CIn').val('');
         $('#datepicker_COut').val('');
     }
-}   
+}
+function date(f) {
+    var cin = document.getElementById("datepicker_CIn").value;
+    var cout = document.getElementById("datepicker_COut").value;
+    if (cout < cin) {
+        alert("faild");
+        $('#datepicker_COut').val('');
+    }
+}
--- HMS/static/app/js/valid-date.js
+++ HMS/static/app/js/valid-date.js
@@ -1,22 +1,25 @@
 // DatePicker
-$(function () {
+$(function ()
+{
     $("#datepicker_CIn").datepicker();
     $("#datepicker_COut").datepicker();
     //Pass the user selected date format
-    $("#format").change(function () {
+    $("#format").change(function ()
+    {
         $("#datepicker_CIn").datepicker("option", "dateFormat", $(this).val());
         $("#datepicker_COut").datepicker("option", "dateFormat", $(this).val());
     });
 });
 
-function handler(e) {
+function handler(e)
+{
     var today = new Date();
     var dd = today.getDate();
     var mm = today.getMonth() + 1; //January is 0!
     var yyyy = today.getFullYear();
     today = mm + '/' + dd + '/' + yyyy;
-    selected = e.target.value;
-    if (selected < today) {
+    if (selected < today)
+    {
         alert("Please check the dates you mentioned");
         $('#datepicker_CIn').val('');
         $('#datepicker_COut').val('');
