b450a8b44f6722ae7e78bbbf2a5d6bbad63cd3de
Kamesh Mahendrawada <kameshsaim@gmail.com>
2019-06-02 08:17:41 +0530

    Added new logic for models.py

--- HMS/app/models.py
+++ HMS/app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class UserDetails(models.Model):
     email = models.EmailField(max_length=100,primary_key=True)
@@ -11,64 +12,50 @@ class UserDetails(models.Model):
     def __str__(self):
         return u'%s %s'  % (self.email,self.address)
 
-STATES = (
-        ('Alabama','AL'),('Alaska','AK'),('Arizona','AZ'),('Arkansas','AR'),('California','CA'),
-        ('Colorado','CO'),('Connecticut','CT'),('Delaware','DE'),('Florida','FL'),('Georgia','GA'),
-        ('Hawaii','HI'),('Idaho','ID'),('Illinois','IL'),('Indiana','IN'),('Iowa','IA'),
-        ('Kansas','KS'),('Kentucky','KY'),('Louisiana','LA'),('Maine','ME'),('Maryland','MD'),
-        ('Massachusetts','MA'),('Michigan','MI'),('Minnesota','MN'),('Mississippi','MS'),('Missouri','MO'),
-        ('Montana','MT'),('Nebraska','NE'),('Nevada','NV'),('New Hampshire','NH'),('New Jersey','NJ'),
-        ('New Mexico','NM'),('New York','NY'),('North Carolina','NC'),('North Dakota','ND'),('Ohio','OH'),
-        ('Oklahoma','OK'),('Oregon','OR'),('Pennsylvania','PA'),('Rhode Island','RI'),('South Carolina','SC'),
-        ('South Dakota','SD'),('Tennessee','TN'),('Texas','TX'),('Utah','UT'),('Vermont','VT'),
-        ('Virginia','VA'),('Washington','WA'),('West Virginia','WV'),('Wisconsin','WI'),('Wyoming','WY'),
-    )
-
-class HotelRegister(models.Model):
-    hotel_id = models.AutoField(primary_key=True)
-    hotel_name = models.CharField(max_length=250,unique=True,blank=False)
-    city = models.CharField(max_length=250)
-    state = models.CharField(max_length = 20,choices = STATES,default='Alabama')
-    zip = models.CharField(max_length = 20)
-    email = models.EmailField(max_length = 100)
-    password = models.CharField(max_length = 50)
-    contact = models.IntegerField()
-
-    class Meta:
-        db_table ='hotels'
-
-class HotelBooking(models.Model):
-    hotelid = models.ForeignKey(HotelRegister,on_delete=models.CASCADE)
-    roomtype = models.CharField(max_length = 50)
-    count = models.IntegerField()
-    availability = models.IntegerField()
-
-    class Meta:
-        db_table ='hotelbooking'
-
-
 class RoomBooking(models.Model):
-    ROOM_CHOICES = (
-    ('deluxe','DELUXE'),
-    ('luxury', 'LUXURY'),
-    )
-    checkin  = models.DateField(blank=True)
-    checkout = models.DateField(blank=True)
-    email = models.CharField(max_length=100)
-    hotel_name = models.CharField(max_length = 10)
-    room = models.CharField(max_length=10, choices=ROOM_CHOICES, default='deluxe')
-
+    bookingid = models.BigIntegerField(primary_key=True,default=0)
+    checkin  = models.CharField(max_length=140,default='NULL')
+    checkout = models.CharField(max_length=140,default='NULL') 
+    firstname = models.CharField(max_length=140)
+    middlename = models.CharField(max_length=140)
+    lastname = models.CharField(max_length=140)
+    email = models.EmailField(max_length=50)
+    phone = models.CharField(max_length=10)
+    address = models.TextField(max_length=400)
+    city = models.CharField(max_length=140)
+    state = models.CharField(max_length=140)
+    zipcode = models.IntegerField()
+    idproof = models.CharField(max_length=140)
+    rooms = models.CharField(max_length=40)
+    
     class Meta:
         db_table = 'roombooking'
 
-class BookingInformation(models.Model):
-    firstname = models.CharField(max_length=100)
-    lastname = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
-    phone = models.IntegerField()
-    address = models.CharField(max_length=150)
-    requirements = models.CharField(max_length=100,blank=True)
-    booking_id = models.CharField(max_length=100,primary_key=True)
+class BookingHistory(models.Model):
+    bookingid = models.BigIntegerField(primary_key=True,default=0)
+    checkin  = models.CharField(max_length=140,default='NULL')
+    checkout = models.CharField(max_length=140,default='NULL')
+    email = models.EmailField(max_length=50)
+    userid = models.BigIntegerField(default=0)
 
     class Meta:
-        db_table = 'bookinginfo'
+        db_table = 'bookinghistory'
+
+class Reservation(models.Model):
+    bookingid = models.BigIntegerField(primary_key=True,default=0)
+    checkin  = models.CharField(max_length=140,default='NULL')
+    checkout = models.CharField(max_length=140,default='NULL')
+    email = models.EmailField(max_length=50)
+    amount = models.BigIntegerField(default=0)
+
+    class Meta:
+        db_table = 'reservation'
+
+class Rooms(models.Model):
+    roomtype  = models.CharField(max_length=140,default='NULL')
+    total = models.BigIntegerField(default=0)
+    available = models.BigIntegerField(default=0)
+
+    class Meta:
+        db_table = 'rooms'
+
